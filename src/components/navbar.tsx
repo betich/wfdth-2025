00159d5126f10186596b537dd14f1c1306dc0f67
betich <betich365@gmail.com>
2025-09-05 06:10:07 +0700

    feat(footer): create footer component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-export function Navbar() {
+export default function Navbar() {
   return (
     <nav className="bg-primary text-white w-full flex items-center justify-between px-4 py-2 min-h-[60px]">
       {/* Hamburger icon */}
