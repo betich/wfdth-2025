4d19893fad1ec86f270131f655d62a499cd0b5b7
betich <betich365@gmail.com>
2025-09-05 06:51:26 +0700

    feat(home): add time schedule section

---
+++ src/data/links.ts
@@ -0,0 +1,7 @@
+export const links = [
+  { name: "HOME", href: "#" },
+  { name: "WFD", href: "#" },
+  { name: "WFDTH", href: "#" },
+  { name: "TIME SCHEDULE", href: "#" },
+  { name: "SPEAKERS", href: "#" },
+];
---
+++ src/data/session.ts
@@ -0,0 +1,29 @@
+export interface Session {
+  title: string;
+  time: string;
+  location: string;
+}
+
+export const sessions: Session[] = [
+  // TODO move to md data in directory
+  {
+    title: "Plenary 1: Session topic",
+    time: "09:00 - 10:30",
+    location: "Main Hall",
+  },
+  {
+    title: "Plenary 1: Session topic",
+    time: "09:00 - 10:30",
+    location: "Main Hall",
+  },
+  {
+    title: "Plenary 1: Session topic",
+    time: "09:00 - 10:30",
+    location: "Main Hall",
+  },
+  {
+    title: "Plenary 1: Session topic",
+    time: "09:00 - 10:30",
+    location: "Main Hall",
+  },
+];
